14ef02caf3907aa4a642ed6cb9411d584f48e8cc
Your Name <voilaanwi1@gmail.com>
2024-01-04 03:28:45 -0600

    added getTags, getFavorites, addFavorties, deleteFavorites, and sellRequest methods in API cla

--- src/API.js
+++ src/API.js
@@ -63,6 +63,98 @@ export default class API{
         });
         return savedPost
     }
+    getHomes = async(search,tagId) => {
+        let url ='/house/'
+        let query= new URLSearchParams()
+        if(tagId){
+            query.append('tag',tagId)
+
+        }
+         if(search){
+            query.append('search',search)
+         }
+         if(query.toString() !== ''){
+            url += '?'+ query.toString()  //url=url?key1=value1&key2=value2  
+         }
+
+         const homes= await api.get(url).then(response=>{
+            return response.data
+         }).catch(error=>{
+            throw new Error(error)
+         })
+         return homes
+    }
+    getHome = async id =>{
+        const homes = await api.get('/house/'+ id +'/').then(response=>{ // /house/2/
+            return response.data
+        }).catch(error =>{
+            throw new Error(error)
+        })
+        return homes
+       
+    } 
+
+    getFavorites = async () => {
+        const favorites = await api
+        .get('/favorites/',{requireToken:true})
+        .then(response =>{
+            return response.data
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
+        return favorites
+    }
+
+    addFavorites = async (addFavoritesBody) => {
+        const savedPost = await api
+        .post('/favorites/add/', addFavoritesBody, {requireToken: true})
+        .then(response => {
+            return response.data
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
+        return savedPost
+        
+    }
+
+    deleteFavorites = id => {
+        return api.delete(`favorites/delete/${id}`, {requireToken:true})
+    }
+
+    sellRequest = async data => {
+        const savedPost = await api
+        .post('/sellRequest/add/',
+        {
+            address: data.address,
+            sqft: data.sqft,
+            age_building: data.age_building
+        },
+        {requireToken: true}
+        )
+        .then(response => {
+            return response.data
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
+        return savedPost
+        
+    }
+
+    getTags = async () => {
+        const tags= await api 
+        .get('/tags/')
+        .then (response => {
+            return response.data
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
+        return tags
+
+    }
 
 }
 
